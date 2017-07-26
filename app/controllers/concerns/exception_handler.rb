04601ba4b167a8beb0499ff36c5cc3198367eba2
OGDubjazz <omar__yousry@hotmail.com>
2017-07-26 13:09:31 +0200

    Controllers

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -1,10 +1,11 @@
 module ExceptionHandler
-  # provides the more graceful `included` method
   extend ActiveSupport::Concern
 
+  # Define custom error subclasses - rescue catches `StandardErrors`
   class AuthenticationError < StandardError; end
   class MissingToken < StandardError; end
   class InvalidToken < StandardError; end
+  class ExpiredSignature < StandardError; end
 
   included do
     # Define custom handlers
@@ -12,15 +13,16 @@ module ExceptionHandler
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
     rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
     rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
+    rescue_from ExceptionHandler::ExpiredSignature, with: :four_ninety_eight
+
     rescue_from ActiveRecord::RecordNotFound do |e|
       json_response({ message: e.message }, :not_found)
     end
-
-    rescue_from ActiveRecord::RecordInvalid do |e|
-      json_response({ message: e.message }, :unprocessable_entity)
-    end
   end
 
+  private
+
+  # JSON response with message; Status code 422 - unprocessable entity
   def four_twenty_two(e)
     json_response({ message: e.message }, :unprocessable_entity)
   end
@@ -29,5 +31,8 @@ module ExceptionHandler
   def unauthorized_request(e)
     json_response({ message: e.message }, :unauthorized)
   end
-  
+
+  def four_ninety_eight(e)
+    json_response({ message: e.message }, :invalid_token)
+end
 end
