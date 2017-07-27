85d23cc76793fa848d14fc16a4a46e3b1ac98bfc
OGDubjazz <omar__yousry@hotmail.com>
2017-07-27 10:03:51 +0200

    Versioning

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,9 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'database_cleaner'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
-# Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
@@ -19,30 +16,21 @@ end
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   #config.include RequestSpecHelper, type: :request
   config.include RequestSpecHelper
   config.include ControllerSpecHelper
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+
   config.use_transactional_fixtures = true
   config.include FactoryGirl::Syntax::Methods
 
- # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
   end
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 
-  # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
-  # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
 
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
